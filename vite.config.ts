a38c2994cf7c541934acfa6de24398d1a3efc7be
OlhaRyzha <104225380+OlhaRyzha@users.noreply.github.com>
2025-07-12 15:33:19 +0300

    chore: prepare release

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
-}) 
+});
