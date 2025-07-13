a2d90c9acde7293f412eb970c9ce1717b8b493dc
OlhaRyzha <104225380+OlhaRyzha@users.noreply.github.com>
2025-07-13 13:47:40 +0300

    Release: publish UI and deploy Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,16 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-import path from 'path';
+import { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-a11y'],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
+  addons: ['@storybook/addon-essentials'],
+  framework: { name: '@storybook/react-vite', options: {} },
   viteFinal: async (config) => {
     config.resolve ||= {};
     config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname, '../src'),
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, '../src'),
     };
     return config;
   },
 };
-
 export default config;
